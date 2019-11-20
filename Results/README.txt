85219b6ad4148f5bed2d8284ddfb74e291fbed83
Stefano De Filippis <stfn.defilippis@gmail.com>
2019-11-20 15:04:14 +0100

    Modified the main simulation scrip in order to consider always present the control points'task or only when their distances from the obstacle is below a certain threshold

---
+++ Results/README.txt
@@ -0,0 +1,9 @@
+FAR OBSTACLE CASE
+- obstacle position -> [0.1250,0.9,0.5250]
+- goal position -> [-0.1,0.5,0.5]
+NEAR OBSTACLE CASE
+- obstacle position -> [0.4,0.25,0.4]
+- goal position -> [-0.1,0.5,0.5]
+OBSTACLE ON PATH CASE
+- obstacle position -> [0.1750,0.2,0.5250]
+- goal position -> [-0.1,0.5,0.5]
--- plot_results.py
+++ plot_results.py
@@ -4,14 +4,25 @@ import pickle
 from mpl_toolkits.mplot3d import Axes3D
 joint_angles_file = open('/home/stefano/ROB2_project/Results/obstacle_on_path/joint_angles.txt', 'r')
 joint_velocities_file = open('/home/stefano/ROB2_project/Results/obstacle_on_path/velocities.txt', 'r')
+ee_task_error_file = open('/home/stefano/ROB2_project/Results/obstacle_on_path/ee_task_error.txt', 'r')
+elbow_task_error_file = open('/home/stefano/ROB2_project/Results/obstacle_on_path/elbow_task_error.txt', 'r')
 joint_angles = [[],[],[],[],[],[],[]]
 joint_velocities = [[],[],[],[],[],[],[]]
+ee_task_error = []
+elbow_task_error = []
 for line_angles,line_velocities in zip(joint_angles_file,joint_velocities_file):
     l_angles = line_angles.strip("\n").split(";")
     l_velocities = line_velocities.strip("\n").split(";")
     for i in range(len(l_angles)):
         joint_angles[i].append(float(l_angles[i]))
         joint_velocities[i].append(float(l_velocities[i]))
+for line_ee,line_elbow in zip(ee_task_error_file,elbow_task_error_file):
+    ee_error = line_ee.strip("\n")
+    elbow_error = line_elbow.strip("\n")
+    ee_task_error.append(float(ee_error))
+    elbow_task_error.append(float(elbow_error))
+
+
 handles = []
 for i in range(len(joint_angles)):
     label_joint = "joint "+str(i+1);
@@ -25,4 +36,8 @@ for i in range(len(joint_velocities)):
     h, = plt.plot(range(len(joint_velocities[i])),joint_velocities[i], label = label_joint)
     handles.append(h)
 plt.legend(handles=handles, loc='lower right')
+plt.show()
+plt.plot(range(len(ee_task_error)),ee_task_error)
+plt.show()
+plt.plot(range(len(elbow_task_error)),elbow_task_error)
 plt.show()
