447be2766e5346afc2d947cd344e3ee755eb02be
MarMen1713013 <menchetti.1713013@studenti.uniroma1.it>
2019-12-11 00:38:16 +0100

    Almost final first version: waiting for third control point position and last checks

---
+++ report/tex/plots/allPlots.m
@@ -0,0 +1,27 @@
+mainDir = '/home/marco/Scrivania/ROB2_project';
+al = '/Results/control_points_always_presnt';
+nAl = '/Results/control_points_not_always_present';
+far = '/far_obstacle';
+near = '/obstacle_close_to_path';
+on = '/obstacle_on_path';
+whereIs = {al,nAl};
+whoIs = {far,near,on};
+
+go = {'/report/tex/plots/far','/report/tex/plots/near','/report/tex/plots/on'};
+to = {'/always','/notAlways'};
+
+Titles = {'control_points_distances','ee_task_error','elbow_task_error','joint_angles','joint_velocities'};
+for w=1:3
+    for W=1:2
+        disp([mainDir,whereIs{W},whoIs{w}])
+        plots([mainDir,whereIs{W},whoIs{w}]);
+        pause(0.001);
+        printPdf;
+        %movefile('*.eps',[mainDir,go{w},to{W}])
+        for file=1:5
+            movefile([Titles{file},'.eps'],[mainDir,go{w},to{W}])
+        end
+        disp(['to: ',mainDir,go{w},to{W}]);
+        close all;
+    end
+end
--- report/tex/plots/plots.m
+++ report/tex/plots/plots.m
@@ -1,23 +1,56 @@
-clear;
-close;
-clc;
-dir = input('working directory: ','s');
+function plots(dir)
 switching = readmatrix([dir,'/switching_instants.txt']);
 % TOBE removed:
-switching = switching(:,1);
+switching;% = switching(:,1);
+boolVal = isempty(switching);
+if(~boolVal) 
+    switching = switching(:,1);
+end
 files = {'cps_distances.txt','ee_task_error.txt','elbow_task_error.txt','joint_angles.txt','joint_velocities.txt'};
-
+titles = {'control points distances','ee task error','elbow task error','joint angles','joint velocities'};
+T = 0.005;
+t = linspace(1,152,152)*T;
 for i = 1:5
    data = readmatrix([dir,'/',files{i}]);
    y_max = max(max(data));
    y_min = min(min(data));
    h = figure(i);
+   h.Renderer = 'Painters';
    [row_data,col_data] = size(data);
    for j = 1:col_data
-      plot(data(:,j),'LineWidth',2);
+      plot(t,data(:,j),'LineWidth',2);
       hold on;
    end
-   plot([switching(:),switching(:)],[y_max*1.2,y_min*0.8],'Color',[0 0 0]+0.70,'LineWidth',.5);
-   title(files{i});
-   axis([0,row_data,y_min*0.8,y_max*1.2])
+   if(~boolVal) 
+       for j=1:length(switching)
+       plot(T*[switching(j),switching(j)],[y_max*1.2,y_min*0.8],'Color',[0 0 0]+0.60,'LineWidth',.5);
+       end
+   end
+   title(titles{i});
+   %axis([0,row_data,y_min*0.8,y_max*1.2])
+   grid on;
+   xlabel('time [s]')
+end
+
+figure(1);
+lg=legend('point_1','point_2','point_3');
+lg.Location = 'best';
+ylabel('distance [m]')
+
+figure(2)
+ylabel('error [m]');
+
+figure(3)
+ylabel('error [°]');
+
+figure(4);
+lg=legend('joint 1','joint 2','joint 3','joint 4','joint 5','joint 6','joint 7');
+set(lg,'color','none','Location','best');
+ylabel('[rad]');
+
+figure(5);
+lg=legend('joint 1','joint 2','joint 3','joint 4','joint 5','joint 6','joint 7');
+set(lg,'color','none');
+lg.Location = 'best';
+ylabel('[rad/s]');
 end
